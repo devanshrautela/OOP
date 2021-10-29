b8395ce1a1aa68b2943c9a9fdbf7b591daa0a573
Devansh Rautela <maildevanshrautela@gmail.com>
2021-10-29 08:02:21 +0530

    commit

--- Practical-07/task1/task1.cpp
+++ Practical-07/task1/task1.cpp
@@ -3,22 +3,25 @@ using namespace std;
 int main()
 {
  int ch;
- cout<<"enter the choice :1 year,2 month,3 date"<<endl;
+ cout<<"enter the choice :"<<endl<<"1.Year"<<endl<<"2.Month"<<endl<<"3.date"<<endl<<"4.Exit"<<endl;
  cin>>ch;
- switch(ch)
- {
+    switch(ch){
    case 1:
       cout<<"2021"<<endl;
+      ch=main();
       break;
    case 2:
       cout<<"october"<<endl;
+      ch=main();
       break;
    case 3:
       cout<<"wed 13"<<endl;
+      ch=main();
       break;
+   case 4:
+      return 0;
    default:
       cout<<"invalid choice"<<endl;
-      break;
- }
- return 0;
+      ch=main();
+      break;}
 }
